9d5215d56b420d2ff37324730d664de23f69c742
artyomdrobysh <artyom.drobysh96@gmail.com>
2024-03-17 15:54:57 +0300

    Resolve conflicts

--- oauth2-front-end/src/utils/keycloak.ts
+++ oauth2-front-end/src/utils/keycloak.ts
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
     url: process.env.KEYCLOAK_URL,
-    realm: process.env.KEYCLOAK_REALM,
-    clientId: process.env.KEYCLOAK_CLIENT_ID,
+    realm: process.env.KEYCLOAK_REALM ?? "",
+    clientId: process.env.KEYCLOAK_CLIENT_ID ?? "",
 });
 
 export default keycloak;
